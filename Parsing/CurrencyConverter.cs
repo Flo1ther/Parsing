c4fd450987edb3cfa602d5152ea2e8516923c6ef
Flo1ther <shevchuk.nirosla@gmail.com>
2024-10-05 13:41:17 +0300

    Добавьте файлы проекта.

---
+++ Parsing/CurrencyConverter.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing
+{
+    internal class CurrencyConverter
+    {
+        private static decimal usdRate = 0;
+
+        public static async Task GetUsdRate()
+        {
+            string url = "https://bank.gov.ua/ua/markets/exchangerates";
+
+            HttpClient client = new HttpClient();
+            string html = await client.GetStringAsync(url);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var usdNode = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"exchangeRates\"]/tbody/tr[8]/td[5]");
+
+            if (usdNode != null)
+            {
+                string usdRateText = usdNode.InnerText.Trim().Replace('.', ',');
+
+                if (decimal.TryParse(usdRateText, out decimal parsedRate))
+                {
+                    usdRate = parsedRate;
+                    Console.WriteLine($"Курс USD: {usdRate}");
+                }
+                else
+                {
+                    Console.WriteLine("Помилка перетворення курсу USD.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не вдалося знайти інформацію про курс USD на сайті.");
+            }
+        }
+
+        public static decimal ConvertToUsd(decimal grn)
+        {
+            if (usdRate == 0)
+            {
+                Console.WriteLine("Курс USD не встановлено. Неможливо здійснити конвертацію.");
+                return 0;
+            }
+            return grn / usdRate;
+        }
+    }
+}
---
+++ Parsing/Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using Parsing;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+    await CurrencyConverter.GetUsdRate();
+
+    Console.WriteLine("Введіть суму в гривнях для конвертації в долари США:");
+
+    string input = Console.ReadLine();
+    if (decimal.TryParse(input, out decimal grnAmount))
+    {
+        decimal usdAmount = CurrencyConverter.ConvertToUsd(grnAmount);
+
+        if (usdAmount != 0)
+        {
+            Console.WriteLine($"Сума в доларах США: {usdAmount:F2} USD");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Помилка: введене значення не є числом. Спробуйте ще раз.");
+    }
